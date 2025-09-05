404c525f71d1b47a8a1bc526849ed1e75262ae16
nudugi <39729618+nudugi@users.noreply.github.com>
2025-09-05 22:41:05 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AppMain from "./AppMain";
-import PaymentTest from "./PaymentTest";
 
 export default function App() {
   return (
